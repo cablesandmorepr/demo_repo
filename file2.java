2d0ee70816f1cddc4fe2cdfc2007685210ee68c1
Luis <cablesandmorepr@hotmail.com>
2020-08-04 16:21:25 +0000

    meanigfull modification

--- file2.java
+++ file2.java
@@ -22,5 +22,9 @@ var
 Making changes on feature1 branch
 demo conflict changes demo from master
 making a change in feature 3 branch to demo conflicting merge
+<<<<<<< HEAD
 junk modification -- will undo the change
 meaningfull modification
+=======
+
+>>>>>>> parent of e5f2e26... Junk modification - will undo the the change
