2b2d71541015811903d983360cfa0d39674de0c1
Luis <cablesandmorepr@hotmail.com>
2020-08-04 16:26:01 +0000

    Revert "Junk modification - will undo the the change"
    This reverts commit e5f2e269b2a478bbf73ddedeb396862ab50cd53d.
    
    Conflicts:
    	file2.java

--- file2.java
+++ file2.java
@@ -23,8 +23,12 @@ Making changes on feature1 branch
 demo conflict changes demo from master
 making a change in feature 3 branch to demo conflicting merge
 <<<<<<< HEAD
+<<<<<<< HEAD
 junk modification -- will undo the change
 meaningfull modification
+=======
+
+>>>>>>> parent of e5f2e26... Junk modification - will undo the the change
 =======
 
 >>>>>>> parent of e5f2e26... Junk modification - will undo the the change
