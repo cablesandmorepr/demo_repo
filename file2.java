b0d78a0d10dd3e7e8690a2fe0bb12fb3a60e04c2
Luis <cablesandmorepr@hotmail.com>
2020-08-04 16:37:55 +0000

    Another to commit

--- file2.java
+++ file2.java
@@ -23,15 +23,17 @@ Making changes on feature1 branch
 demo conflict changes demo from master
 making a change in feature 3 branch to demo conflicting merge
 <<<<<<< HEAD
-<<<<<<< HEAD
 junk modification -- will undo the change
 <<<<<<< HEAD
 meaningfull modification
 =======
 
 >>>>>>> parent of e5f2e26... Junk modification - will undo the the change
+<<<<<<< HEAD
 =======
 
 >>>>>>> parent of e5f2e26... Junk modification - will undo the the change
 =======
 >>>>>>> parent of 11c8c05... meanigfull modification
+=======
+>>>>>>> parent of 2b2d715... Revert "Junk modification - will undo the the change"
